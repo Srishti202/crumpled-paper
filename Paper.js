fa7a6ab90f54b5e47543000d2d2218d2b5847d5b
Srishti202 <80098659+Srishti202@users.noreply.github.com>
2021-06-11 15:41:15 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,32 @@
+class Paper
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:false	,
+            restitution : 0.6		
+			}
+            this.image = loadImage("paper.png")
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+	//		translate(paperPos.x, paperPos.y);
+			imageMode(CENTER)
+		image(this.image,paperPos.x,paperPos.y,this.w , this.h )
+    
+			pop()
+			
+	}
+
+}
